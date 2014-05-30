7ed78e77f0681a5115a3b7f082d7aafa1f909216
Rachel S. Wang <rachel.wang@rachel-wang-ltm.internal.salesforce.com>
2014-05-30 15:22:27 -0700

    Add pass/fail with chance

--- routes/index.js
+++ routes/index.js
@@ -21,13 +21,28 @@ router.get('/validation', function(req, res) {
     res.render('validation', { title: 'Validation', validate: 'Passed!' })
 });
 
+/* GET Fail page. */
+router.get('/fail', function(req, res) {
+    res.render('fail', { title: 'Validation', validate: 'Failed!' })
+});
+
+/* GET Pass page. */
+router.get('/pass', function(req, res) {
+    res.render('pass', { title: 'Validation', validate: 'Passed!' })
+});
+
 
 /* POST to Add User Service */
 router.post('/submit', function(req, res) {
-    // If it worked, set the header so the address bar doesn't still say /adduser
-    res.location("validation");
-    // And forward to success page
-    res.redirect("validation");
+    x = Math.random();
+    if (x < 0.1) {
+        res.location("pass");
+        res.redirect("pass");
+    }
+    else {
+        res.location("fail");
+        res.redirect("fail");
+    }
 });
 
 module.exports = router;
